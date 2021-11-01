b91b1ad0d35329e825e3cf541295ab3e264f6da7
Unknown <tyler.simoni.8@gmail.com>
2021-11-01 00:06:15 -0400

    Updating Redirect URI to domain

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 const Spotify = {
   CLIENT_ID: '26779370bf7d48fc9cb8127014196126',
-  REDIRECT_URI: 'http://localhost:3000/',
+  REDIRECT_URI: 'http://peanut-butter-and-jammming.surge.sh',
   AUTHORIZE_URL: 'https://accounts.spotify.com/authorize',
 };
 
